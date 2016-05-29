26dd47eb015f91390d0cc71118d66182e9067f12
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-05-29 14:35:29 -0400

    movies have a name.

--- src/main/java/movielist/Movie.java
+++ src/main/java/movielist/Movie.java
@@ -2,10 +2,16 @@ package movielist;
 
 public class Movie {
 
+    private String name;
+
     public Movie() {
     }
 
     public Movie(String title) {
+        this.name = title;
+    }
 
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/movielist/test/TestMovie.java
+++ src/test/java/movielist/test/TestMovie.java
@@ -1,7 +1,19 @@
 package movielist.test;
 
+import movielist.Movie;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by wilsoncampusano on 5/29/16.
  */
 public class TestMovie {
+
+    @Test
+    public void testMovieName() {
+        Movie starWars = new Movie("star Wars");
+        assertEquals("star wars should have a name", "star Wars", starWars.getName());
+
+    }
 }
