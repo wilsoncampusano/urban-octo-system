63aa9c4b93bacfc0cfaab0fd7fd7b99c0fa6f5b9
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2016-05-28 20:21:05 -0400

    test suite

---
+++ src/test/java/movielist/test/AllTest.java
@@ -0,0 +1,14 @@
+package movielist.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses(value ={
+        TestEmptyMovieList.class,
+        TestMovieListWithOneMovie.class,
+        TestMovieListWithTwoMovies.class
+})
+@RunWith(Suite.class)
+public class AllTest {
+
+}
